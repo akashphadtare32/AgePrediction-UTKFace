4351ba3a4077443786204f1ef7dd4513613e2bf8
Moritz Mistol <moritz.mistol@gmail.com>
2023-04-26 19:52:30 +0200

    update repo

--- src/prepare.py
+++ src/prepare.py
@@ -1,1 +1,7 @@
 """Functions for training preparation."""
+import tensorflow as tf
+
+
+def load_dataset() -> tf.data.Dataset:
+    """Load the data into a Tensorflow Dataset."""
+    pass
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,26 +1,19 @@
 """Test the config setup."""
+import os
+
 from hydra import compose, initialize
 
 
-# 1. initialize will add config_path the config search path within the context
-# 2. The module with your configs should be importable.
-#    it needs to have a __init__.py (can be empty).
-# 3. THe config path is relative to the file calling initialize (this file)
 def test_with_initialize() -> None:
     """Test the config setup with initialize (compose API)."""
     with initialize(version_base="1.3", config_path="../src/conf"):
         # config is relative to a module
+        os.environ["WANDB_API_KEY"] = "test"
         cfg = compose(
             config_name="config",
             overrides=[
                 "wandb.project=test_project",
-                "wandb.mode=offline",
-                "wandb.group=test_group",
             ],
         )
-        assert cfg.wandb == {
-            "project": "test_project",
-            "mode": "offline",
-            "group": "test_group",
-            "name": None,
-        }
+        assert cfg.wandb.project == "test_project"
+        assert cfg.wandb.key == "test"
