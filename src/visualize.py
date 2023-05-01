02b237a3ee9db8fd84688e3cfe0dadaef28a8a88
Moritz Mistol <moritz.mistol@gmail.com>
2023-05-01 17:39:34 +0200

    add visualizations

--- src/visualize.py
+++ src/visualize.py
@@ -1,1 +1,31 @@
 """Visualizations module."""
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+
+def visualize_dataset(ds: tf.data.Dataset, figsize=(8, 8)) -> mpl.figure.Figure:
+    """Visualize the dataset."""
+    fig = plt.figure(figsize=figsize)
+    for i, (image, label) in enumerate(ds.take(9)):
+        _ = plt.subplot(3, 3, i + 1)
+        plt.imshow(image.numpy().astype("int32"))
+        plt.title(int(label))
+        plt.axis("off")
+    plt.tight_layout()
+    return fig
+
+
+def visualize_augmented_image(
+    train_ds: tf.data.Dataset, data_augmentation: tf.keras.Sequential, figsize=(8, 8)
+) -> mpl.figure.Figure:
+    """Visualize the augmented images."""
+    fig = plt.figure(figsize=figsize)
+    for image, _ in train_ds.take(1):
+        for i in range(9):
+            _ = plt.subplot(3, 3, i + 1)
+            augmented_image = data_augmentation(tf.expand_dims(image, 0), training=True)
+            plt.imshow(augmented_image[0].numpy().astype("int32"))
+            plt.axis("off")
+    plt.tight_layout()
+    return fig
