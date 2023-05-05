66df516cfceb152a5699a6e50cb48243ef6e7978
Moritz Mistol <moritz.mistol@gmail.com>
2023-05-05 10:31:41 +0200

    update baseline model

--- src/custom_models/baseline_cnn.py
+++ src/custom_models/baseline_cnn.py
@@ -18,11 +18,16 @@ def get_base_model(input_shape):
             Conv2D(32, kernel_size=(3, 3), activation="relu"),
             Conv2D(32, kernel_size=(3, 3), activation="relu"),
             MaxPooling2D(),
+            Dropout(0.25),
             Conv2D(64, kernel_size=(3, 3), activation="relu"),
             Conv2D(64, kernel_size=(3, 3), activation="relu"),
             MaxPooling2D(),
-            Flatten(),
             Dropout(0.25),
+            Conv2D(128, kernel_size=(3, 3), activation="relu"),
+            Conv2D(128, kernel_size=(3, 3), activation="relu"),
+            MaxPooling2D(),
+            Dropout(0.25),
+            Flatten(),
         ]
     )
     return model
