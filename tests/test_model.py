b7d628399432ab80e581436dec4b346858f5b88e
Moritz Mistol <moritz.mistol@gmail.com>
2023-05-15 09:48:04 +0200

    update tests for change in build_model_from_cfg

--- tests/test_model.py
+++ tests/test_model.py
@@ -15,7 +15,14 @@ from src.model import (
 def efficientnet_config():
     """Return the config for the EfficientNet model."""
     with initialize(version_base="1.3", config_path="../src/conf"):
-        cfg = compose(config_name="config", overrides=["model=efficientnetv2"])
+        cfg = compose(
+            config_name="config",
+            overrides=[
+                "model=efficientnetv2",
+                "model.num_finetune_layers=all",
+                "optimizer=adam",
+            ],
+        )
         return cfg
 
 
@@ -27,7 +34,7 @@ def test_instantiate_efficientnet(efficientnet_config):
 
 def test_build_from_cfg(efficientnet_config):
     """Test that the model can be built from the config dict."""
-    model = build_model_from_cfg(efficientnet_config, first_stage=True)
+    model, base_model = build_model_from_cfg(efficientnet_config, first_stage=True)
     assert model.__class__.__name__ == "Functional"
     assert model.optimizer.__class__.__name__ == "Adam"
 
@@ -37,11 +44,17 @@ def test_build_from_cfg(efficientnet_config):
         == actual_lr
     )
 
+    for layer in base_model.layers:
+        # should be frozen because `model.freeze_base=True`
+        assert layer.trainable is False
+
 
 def test_build_from_cfg_stage2(efficientnet_config):
     """Test that the stage-2 model can be built from the config dict."""
-    model = build_model_from_cfg(efficientnet_config, first_stage=True)
-    model = build_model_from_cfg(efficientnet_config, model=model, first_stage=False)
+    model, base_model = build_model_from_cfg(efficientnet_config, first_stage=True)
+    model, base_model = build_model_from_cfg(
+        efficientnet_config, model=model, base_model=base_model, first_stage=False
+    )
     assert model.__class__.__name__ == "Functional"
     assert model.optimizer.__class__.__name__ == "Adam"
 
@@ -49,6 +62,11 @@ def test_build_from_cfg_stage2(efficientnet_config):
 
     optim_config = model.optimizer.get_config()
     assert optim_config["learning_rate"]["config"]["initial_learning_rate"] == actual_lr
+
+    for layer in base_model.layers:
+        # in second, stage, all layers should be trainable
+        # because `model.num_finetune_layers=all`
+        assert layer.trainable is True
 
 
 def test_no_preprocessing(efficientnet_config):
